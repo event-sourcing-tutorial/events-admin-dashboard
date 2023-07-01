fb499a1ad475fe5a254246f71694c04bd68fb517
Abdulaziz Ghuloum <aghuloum@gmail.com>
2023-07-01 10:14:54 +0200

    added codemirror

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import {Stack} from '@mui/system';
+import {Container, TextField} from '@mui/material';
+import CodeMirror from '@uiw/react-codemirror';
+import { json } from '@codemirror/lang-json';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+  components: {
+    MuiInputBase: {
+      styleOverrides: {
+        root: {
+          fontFamily: ["Menlo", "Ubuntu Mono", "Monaco", "monospace"].join(","),
+        },
+      }
+    },
+  },
+});
+
+const Editor = () => 
+  <CodeMirror
+      value="{}"
+  //minHeight="200px"
+      extensions={[json()]}
+      theme="dark"
+      editable={true}
+      basicSetup={{
+        lineNumbers: false,
+      }}
+      onChange={(value) => {
+        console.log(value);
+      }}
+   />;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <main><MyApp /></main>
+    </ThemeProvider>
   );
 }
+
+
+function MyApp() {
+  return (
+    <Container maxWidth="sm">
+      <Stack direction="column" spacing={2}>
+        <TextField fullWidth multiline rows={4}  />
+        <Editor />
+        <Button variant="contained">Hello World</Button>
+        <Button variant="contained">Hello World</Button>
+        <Button variant="contained">Hello World</Button>
+      </Stack>
+    </Container>
+  );
+}
+
 
 export default App;
